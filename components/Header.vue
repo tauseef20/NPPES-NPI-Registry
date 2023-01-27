d2ffd64d1f6cc43520200e3ff01b999ad30176cf
tauseef20 <tauseeef205@gmail.com>
2023-01-27 10:57:15 +0500

    navbar

---
+++ components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <a-row class="headerRow pt-2" type="flex">
+        <a-col align="middle" :span="12">NPPES NPI Registry</a-col>
+        <a-col align="middle" :span="12">
+        <a-row class="leftRow">
+            <a-col align="middle" :span="4">NPPES</a-col>
+            <a-col align="middle" :span="4">Downloads</a-col>
+            <a-col align="middle"  :span="3">API</a-col>
+            <a-col align="middle" :span="2">Help</a-col>
+        </a-row>
+        </a-col>
+    </a-row>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.headerRow{
+    background: #085FA1;
+    height: 55px;
+    color: #fff;
+    /* font-weight: bold; */
+    font-size: 20px;
+}
+.leftRow{
+    font-size: 20px;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="components-layout-demo-basic">
+    <Header/>
+    <a-row>
+        <a-col>
+            <nuxt/>
+        </a-col>
+    </a-row>
+    
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue"
+export default {
+  components: {
+    Header,
+   
+  },
+};
+</script>
+
+<style>
+
+</style>
